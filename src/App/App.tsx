0d076544d5f5236da25f44a15a3f043adc9334f3
Danniel Hansel <hansel.danniel@gmail.com>
2026-05-05 20:39:41 -0500

    created first scene for Login Form

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,15 +3,45 @@ import './App.scss'
 import { Header } from './Header/Header'
 import { Simple } from './Simple/Simple'
 import { LoginForm } from './LoginForm/LoginForm'
+import { actions, kea, path, reducers, useValues } from 'kea'
+import type { sceneLogicType } from './AppType'
+
+export enum Scene {
+  LoginForm = 'LoginForm',
+  Simple = 'Simple',
+}
+
+const scenes: Record<Scene, () => JSX.Element> = {
+  [Scene.LoginForm]: () => <LoginForm />,
+  [Scene.Simple]: () => <Simple />,
+}
+
+export const sceneLogic = kea<sceneLogicType>([
+  path(['App', 'App']),
+  actions({
+    // using actions that has a payload.
+    setScene: (scene: Scene) => ({ scene }), // this setScene is gonna take the scene as an argument and return an object with the scene, this is how you define actions with payloads in kea, it is a function that takes an argument and returns an object that has key scene.
+  }),
+  reducers({
+    scene: [
+      Scene.LoginForm as Scene,
+      {
+        setScene: (_, { scene }) => scene,
+      },
+    ],
+  }),
+])
 
 export function App() {
+  const { scene } = useValues(sceneLogic)
+  console.log('Current scene:', scene)
   return (
-    <div className="App">
+    < div className="App" >
       <Header />
       <div className="App-layout">
-        {/* <Simple /> */}
+        <Simple />
         <LoginForm />
       </div>
-    </div>
+    </div >
   )
 }
